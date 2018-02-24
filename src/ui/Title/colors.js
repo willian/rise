bb9cabf19c144ddb782a6791c79b579a2c55260a
Willian Fernandes <willian@willianfernandes.com.br>
2018-02-24 17:27:13 -0300

    Add a Title UI component

---
+++ src/ui/Title/colors.js
@@ -0,0 +1,5 @@
+import Color from 'color'
+
+export default {
+  default: Color('#313537')
+}
---
+++ src/ui/Title/index.js
@@ -0,0 +1,15 @@
+import glamorous from 'glamorous'
+
+import colors from './colors'
+
+const Title = glamorous.h2({
+  color: colors.default,
+  fontSize: '32px',
+  fontFamily: 'Lato',
+  fontWeight: '700',
+  lineHeight: '40px',
+  margin: 0,
+  verticalAlign: 'baseline'
+})
+
+export default Title
